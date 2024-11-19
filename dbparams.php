75fcf64df87792e2442e9c4e0690f5f588b26d94
No Name <49948225+playking@users.noreply.github.com>
2024-11-19 21:28:07 +0300

    a template for connecting to db

---
+++ dbparams.php
@@ -0,0 +1,7 @@
+<?php
+    $host = ;
+    $port = ;
+    $dbname = ;
+    $user = ;
+    $password = ;
+?>
---
+++ env.php
@@ -0,0 +1,11 @@
+<?php
+// Пример содержимого env.php
+$host = ;
+$port = ;
+$dbname = ;
+$user = ;
+$password = ;
+
+$DB_CONNECTION_STRING = "host=$host port=$port dbname=$dbname user=$user password=$password";
+?>
+
